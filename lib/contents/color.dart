0003a0177e7487c313dca64f64c382c5402e40c7
BEDIN <bedinadejosue39@gmail.com>
2024-03-06 12:20:11 +0000

    create button

---
+++ lib/contents/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const background = Color(0xFFE9D5DA);
+const Color valbutt = Color(0xFF363062);
+const Color color = Color(0xFF4D4C7D);
+const Color button = Color(0xFF827397);
--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/contents/color.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,7 +12,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
-
+      backgroundColor: background,
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(color: Colors.red,)
+          ),
+          Row(
+            children:[
+              Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        primary: button,
+                        padding: EdgeInsets.all(22)
+                      ),
+                      onPressed: () {},
+                      child: Text('1',style: TextStyle(
+                        color:Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                        ),
+                      ),
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
